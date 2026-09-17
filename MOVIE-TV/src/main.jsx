dbb36d335daf300284b2958cbfe25dcb2d7ea278
Isadora <isadora.bassoasseiro@gmail.com>
2026-09-17 08:52:36 -0300

    fix:menu/login color

--- MOVIE-TV/src/main.jsx
+++ MOVIE-TV/src/main.jsx
@@ -4,10 +4,12 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import LoginPage from './pages/LoginPage.jsx'
+import ListaPage from './pages/ListaPage.jsx'
 
 const router = createBrowserRouter([
   {path:"/", element:<App/>},
-  {path:"/login", element:<LoginPage/>}
+  {path:"/login", element:<LoginPage/>},
+  {path:"/lista", element:<ListaPage/>}
   ]);
 
 createRoot(document.getElementById('root')).render(
---
+++ MOVIE-TV/src/pages/ListaPage.jsx
@@ -0,0 +1,12 @@
+const ListaPage = () => {
+    return (
+        <>
+        <div>
+            <h1>Lista de Filmes</h1>
+        </div>
+        </>
+    ) 
+
+}
+
+export default ListaPage
